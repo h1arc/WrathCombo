02aad27a9170d012f67c0144230fcf51847c6f24
Limiana <5073202+Limiana@users.noreply.github.com>
2025-09-22 11:55:58 +0300

    Start something maybe

---
+++ WrathCombo/Services/ActionRequestIPC/ActionRequest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrathCombo.Services.ActionRequestIPC;
+public readonly record struct ActionRequest
+{
+    /// <summary>
+    /// Until what moment this ActionRequest is valid. Value is <see cref="Environment.TickCount64"/>.
+    /// </summary>
+    public readonly long Deadline;
+    /// <summary>
+    /// If not present (0), use it "as is". 
+    /// </summary>
+    public readonly uint TargetEntityID;
+    /// <summary>
+    /// If TargetEntityID is not specified, TargetLocation must be provided for location-based skills. 
+    /// </summary>
+    public readonly Vector3 TargetLocation;
+
+    public ActionRequest(long deadline) : this()
+    {
+        Deadline = deadline;
+    }
+
+    public ActionRequest(long deadline, uint targetEntityID) : this()
+    {
+        Deadline = deadline;
+        TargetEntityID = targetEntityID;
+    }
+
+    public ActionRequest(long deadline, Vector3 targetLocation) : this()
+    {
+        Deadline = deadline;
+        TargetLocation = targetLocation;
+    }
+
+    public bool IsActive => Environment.TickCount64 < Deadline;
+}
---
+++ WrathCombo/Services/ActionRequestIPC/ActionRequestIPCProvider.cs
@@ -0,0 +1,64 @@
+﻿using ECommons.EzIpcManager;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrathCombo.Services.ActionRequestIPC;
+//this is static to maximize performance
+public static class ActionRequestIPCProvider
+{
+    public static Dictionary<ActionDescriptor, List<ActionRequest>> ActionRequests = [];
+    public static Dictionary<ActionDescriptor, List<ActionRequest>> ActionBlacklist = [];
+
+    [EzIPC]
+    public static void RequestBlacklist(ActionType actionType, uint actionID, int timeMs)
+    {
+        ActionDescriptor d = new(actionType, actionID);
+        if(!ActionBlacklist.TryGetValue(d, out var v))
+        {
+            v = [];
+            ActionBlacklist[d] = v;
+        }
+        v.Add(new(Environment.TickCount64 + timeMs));
+    }
+
+    [EzIPC]
+    public static void RequestAction(ActionType actionType, uint actionID, int timeMs)
+    {
+        ActionDescriptor d = new(actionType, actionID);
+        if(!ActionRequests.TryGetValue(d, out var v))
+        {
+            v = [];
+            ActionRequests[d] = v;
+        }
+        v.Add(new(Environment.TickCount64 + timeMs));
+    }
+
+    [EzIPC]
+    public static void RequestActionOnTarget(ActionType actionType, uint actionID, uint targetEntityId, int timeMs)
+    {
+        ActionDescriptor d = new(actionType, actionID);
+        if(!ActionRequests.TryGetValue(d, out var v))
+        {
+            v = [];
+            ActionRequests[d] = v;
+        }
+        v.Add(new(Environment.TickCount64 + timeMs, targetEntityId));
+    }
+
+    [EzIPC]
+    public static void RequestActionLocation(ActionType actionType, uint actionID, Vector3 targetLocation, int timeMs)
+    {
+        ActionDescriptor d = new(actionType, actionID);
+        if(!ActionRequests.TryGetValue(d, out var v))
+        {
+            v = [];
+            ActionRequests[d] = v;
+        }
+        v.Add(new(Environment.TickCount64 + timeMs, targetLocation));
+    }
+}
