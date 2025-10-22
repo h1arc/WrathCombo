fd1076226a5dbd2ccbac9d834d8082b23c2323c3
Kage <95875972+Kagekazu@users.noreply.github.com>
2025-10-22 22:34:42 +0200

    better check

--- WrathCombo/Combos/PvE/MCH/MCH.cs
+++ WrathCombo/Combos/PvE/MCH/MCH.cs
@@ -36,7 +36,7 @@ internal partial class MCH : PhysicalRanged
 
                 // Wildfire
                 if (CanApplyStatus(CurrentTarget, Debuffs.Wildfire) &&
-                    ActionReady(Wildfire) && HasWeavedAction(Hypercharge) &&
+                    ActionReady(Wildfire) && JustUsed(Hypercharge, GCD / 2) &&
                     !HasStatusEffect(Buffs.Wildfire))
                     return Wildfire;
 
@@ -282,7 +282,7 @@ internal partial class MCH : PhysicalRanged
                 if (IsEnabled(Preset.MCH_ST_Adv_WildFire) &&
                     (MCH_ST_WildfireBossOption == 0 || TargetIsBoss()) &&
                     CanApplyStatus(CurrentTarget, Debuffs.Wildfire) &&
-                    ActionReady(Wildfire) && HasWeavedAction(Hypercharge) &&
+                    ActionReady(Wildfire) && JustUsed(Hypercharge, GCD / 2) &&
                     !HasStatusEffect(Buffs.Wildfire))
                     return Wildfire;
 
