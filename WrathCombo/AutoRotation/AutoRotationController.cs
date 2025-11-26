e6a0cede4a8cb8f13140aa4e5e29cd1b0a451ad4
Ethan Henderson <ethan@zbee.codes>
2025-11-26 12:14:53 -0700

    fix(AutoRotation): Allow DP-management through

--- WrathCombo/AutoRotation/AutoRotationController.cs
+++ WrathCombo/AutoRotation/AutoRotationController.cs
@@ -691,6 +691,8 @@ internal unsafe static class AutoRotationController
             bool switched = SwitchOnDChole(attributes, outAct, ref target);
             if (outAct is DNC.ClosedPosition && DNC.DancePartnerResolver() is IBattleChara dp)
                 target = dp;
+            if (outAct is DNC.Ending)
+                target = Player.Object;
 
             var canUseSelf = NIN.MudraSigns.Contains(outAct)
                 ? target is not null && target.IsHostile()
