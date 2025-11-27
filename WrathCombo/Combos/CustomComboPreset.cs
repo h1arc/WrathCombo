5e7b72ec8be3efa33dda611d49c118714116886f
Kage <95875972+Kagekazu@users.noreply.github.com>
2025-11-27 14:11:44 +0100

    Add lvl 90 openers

--- WrathCombo/Combos/CustomComboPreset.cs
+++ WrathCombo/Combos/CustomComboPreset.cs
@@ -3876,7 +3876,7 @@ public enum Preset
     MNK_STUseFormShift = 9017,
 
     [ParentCombo(MNK_ST_AdvancedMode)]
-    [CustomComboInfo("Opener Option", "Uses selected opener", Job.MNK)]
+    [CustomComboInfo("Opener Option", "Adds the selected Balance opener at lvl 90/100.", Job.MNK)]
     MNK_STUseOpener = 9006,
 
     [ParentCombo(MNK_ST_AdvancedMode)]
