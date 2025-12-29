fb9426a4f01022c237f6ef2ec7ab1b02557cddc6
Taurenkey <github@taurenkey.co.uk>
2025-12-29 15:23:13 +0000

    More TB

--- WrathCombo/CustomCombo/Functions/Status.cs
+++ WrathCombo/CustomCombo/Functions/Status.cs
@@ -443,7 +443,8 @@ internal abstract partial class CustomComboFunctions
                vfx.Path.StartsWith("vfx/lockon/eff/x6fe_fan100_50_0t1", Lower) || //Necron Blue Shockwave - Cone Tankbuster
                vfx.Path.StartsWith("vfx/common/eff/mon_eisyo03t", Lower) || //M10 Deep Impact AoE TB (also generic?)
                vfx.Path.StartsWith("vfx/lockon/eff/m0676trg_tw_d0t1p", Lower) || //M10 Hot Impact shared TB
-               vfx.Path.StartsWith("vfx/lockon/eff/m0676trg_tw_s6_d0t1p", Lower); //M11 Raw Steel 
+               vfx.Path.StartsWith("vfx/lockon/eff/m0676trg_tw_s6_d0t1p", Lower) || //M11 Raw Steel 
+               vfx.Path.StartsWith("vfx/lockon/eff/z6r2b3_8sec_lockon_c0a1", Lower); //Kam'lanaut Princely Blow
     }
 
     /// <summary>
