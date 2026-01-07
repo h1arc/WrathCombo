6989b5c8f682ba5543ee8361e9c4ddc133bce1ee
Taurenkey <github@taurenkey.co.uk>
2026-01-07 07:42:39 +0000

    Don't switch targets if set to manual mode

--- WrathCombo/AutoRotation/AutoRotationController.cs
+++ WrathCombo/AutoRotation/AutoRotationController.cs
@@ -812,7 +812,7 @@ internal unsafe static class AutoRotationController
             var canUse = (canUseSelf || canUseTarget || areaTargeted) && outAct.ActionAttackType() is { } type && (type is ActionAttackType.Ability || type is not ActionAttackType.Ability && RemainingGCD == 0);
             var isHeal = attributes.AutoAction!.IsHeal;
 
-            if ((!isHeal && cfg.DPSSettings.DPSAlwaysHardTarget) || (isHeal && cfg.HealerSettings.HealerAlwaysHardTarget))
+            if ((!isHeal && cfg.DPSSettings.DPSAlwaysHardTarget && mode is not DPSRotationMode.Manual) || (isHeal && cfg.HealerSettings.HealerAlwaysHardTarget && mode is not HealerRotationMode.Manual))
                 Svc.Targets.Target = target;
 
             var castTime = ActionManager.GetAdjustedCastTime(ActionType.Action, outAct);
